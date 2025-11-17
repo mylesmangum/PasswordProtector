7b308b8e9aef2079c9338273c6530065efdf3dd7
TerminatorDom77 <dominicliechty@gmail.com>
2025-11-17 11:36:24 -0700

    Ask for creation of developer master password

--- src/manager.py
+++ src/manager.py
@@ -1,6 +1,8 @@
 from ast import literal_eval
 # from encrypt_string import symEncrypt, symDecrypt
 import re
+import os
+from dotenv import load_dotenv
 from database_interface import createDatabase, createItem, searchTable
 from pass_encryption import encryptPassword, decryptPassword, checkPassword, createStrongPassword
 
@@ -52,6 +54,14 @@ def searchAccount():
 
 
 def runLoop():
+    if not os.getenv("DEVELOPER_PASSWORD"):
+        createMasterPassword()
+    else:
+        print("Enter the developer password if you are a developer, else just press enter: ")
+        dev_password = input()
+        if dev_password == decryptPassword(os.getenv("DEVELOPER_PASSWORD")):
+            print("You are in developer mode")
+            
     print("If adding a new account, please type \"ADD\"")
     print("If searching for a password, please type \"FIND\"")
     print("To exit, please type \"EXIT\"")
@@ -82,11 +92,28 @@ def invalidCharacter(error_step):
     '''Gives error message for invalid character, argument should be text to let user know where they put the error'''
     print(f"It seems that you used an invalid character during {error_step}, please try again")
 
-def storePasswords(user, name, password):
+def storePasswords(user, name, password, developer_mode=False):
     # decode the byte type to be writable in a text file
-    createItem(user, name, password)
+    if not developer_mode:
+        createItem(user, name, password)
+    else:
+        print("stored!")
+        with open(".env", "w", encoding="utf-8") as env_file:
+            env_file.write(f"DEVELOPER_PASSWORD={password}\n")        
+
+def createMasterPassword():
+    print("Please put in a master password for develper mode: ")
+    raw_pass = input()
+    while not checkPassword(raw_pass):
+        print("The password you have chosen is too common, please choose a different password.")
+        raw_pass = input()
+    while not createStrongPassword(raw_pass):
+        raw_pass = input()
+    encrypted_pass = encryptPassword(raw_pass)
+    storePasswords("developer", "master_password", encrypted_pass, developer_mode=True)
+
     
 if __name__ == "__main__":
-
+    load_dotenv()
     print("Welcome!  Please enter name to continue.")
     runLoop()
